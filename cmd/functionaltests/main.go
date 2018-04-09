4f9e7732bdead91c71280a15e3e04e8a2db40ec3
Unknown <baggerone@gmail.com>
2018-04-09 10:23:28 -0400

    Add cron management for admin tasks and get other tasks via api

--- cmd/functionaltests/main.go
+++ cmd/functionaltests/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/silinternational/speed-snitch-agent"
-	"github.com/silinternational/speed-snitch-agent/lib/speedtestnet"
 	"gopkg.in/robfig/cron.v2"
 	"github.com/silinternational/speed-snitch-agent/lib/tasks"
 	"runtime"
 	"github.com/silinternational/speed-snitch-agent/lib/logqueue"
+	"github.com/silinternational/speed-snitch-agent/lib/adminapi"
+	"fmt"
+	"os"
+	"time"
 )
 
 
@@ -21,80 +24,68 @@ func (f FakeLogger) Process(fakeLogKey, logText string, c ...interface{}) error 
 func main() {
 	println("Function Tests - main.main")
 
+	startTime := time.Now()
+
 	testLogger := FakeLogger{}
 
 	newLogs := make(chan string, 10000)
 
 	go logqueue.Manager(newLogs, "fakeLogKey", &agent.LoggerInstance{testLogger})
 
-	config := getConfig()
+	baseURL := "http://fillup.proxy.beeceptor.com"
+	config, err := adminapi.GetConfig(baseURL)
 
-	mainCron := cron.New()
+	if err != nil {
+		fmt.Printf("\nError getting config from %s\n\t%s", baseURL, err.Error())
+		os.Exit(1)
+	}
 
-	tasks.UpdateTasks(config.Tasks, mainCron, newLogs)
-	mainCron.Start()
+	testCron := cron.New()
+
+	tasks.UpdateTasks(config.Tasks, testCron, newLogs)
+	testCron.Start()
+
+
+	sysCron := cron.New()
+
+	sysCron.AddFunc( // Say Hello every 15 seconds
+		"*/15 * * * * *",
+		func() {
+			adminapi.SayHello(config, startTime)
+		},
+	)
+
+	sysCron.AddFunc( // Get Config every 3 minutes
+		"*/3 * * * *",
+		func() {
+			config, err := adminapi.GetConfig(baseURL)
+			if err != nil {
+				fmt.Printf("\nError getting config from %s\n\t%s", baseURL, err.Error())
+				return
+			}
+			tasks.UpdateTasks(config.Tasks, testCron, newLogs)
+		},
+	)
+
+	sysCron.AddFunc( // Flush log every minute
+		"* * * * *",
+		func() {
+			newLogs <- logqueue.FlushLogQueue
+		},
+	)
+
+	sysCron.Start()
 
 	runtime.Goexit()
+
+
 }
 
-func getConfig() agent.Config {
+func clearCron(thisCron *cron.Cron) {
+	entries := thisCron.Entries()
 
-	return agent.Config{
-		Version: struct {
-			Latest string
-			URL    string
-		}{
-			Latest: "1.0.0",
-			URL:    "https://github.com/silinternational/speed-snitch-agent/raw/1.0.0/dist",
-		},
-		Tasks: []agent.Task{
-			{
-				Type:     agent.TypePing,
-				Schedule: "10,20,30,50 * * * * *", // ping every 10 seconds
-				Data: agent.TaskData{
-					StringValues: map[string]string{
-						speedtestnet.CFG_TEST_TYPE: speedtestnet.CFG_TYPE_LATENCY,
-					},
-					IntValues: map[string]int{
-						speedtestnet.CFG_SERVER_ID: 5029,
-						speedtestnet.CFG_TIME_OUT:  5,
-					},
-					FloatValues: map[string]float64{speedtestnet.CFG_MAX_SECONDS: 6},
-				},
-
-				SpeedTestRunner: agent.SpeedTestInstance{speedtestnet.SpeedTestRunner{}},
-			},
-			{
-				Type:     agent.TypeSpeedTest,
-				Schedule: "40 * * * * *", // ping every 10 seconds
-				Data: agent.TaskData{
-					StringValues: map[string]string{
-						speedtestnet.CFG_TEST_TYPE: speedtestnet.CFG_TYPE_ALL,
-					},
-					IntValues: map[string]int{
-						speedtestnet.CFG_SERVER_ID: 5029,
-						speedtestnet.CFG_TIME_OUT:  5,
-					},
-					FloatValues: map[string]float64{speedtestnet.CFG_MAX_SECONDS: 6},
-					IntSlices: map[string][]int{
-						speedtestnet.CFG_DOWNLOAD_SIZES: {245388, 505544},
-						speedtestnet.CFG_UPLOAD_SIZES:   {32768, 65536},
-					},
-				},
-
-				SpeedTestRunner: agent.SpeedTestInstance{speedtestnet.SpeedTestRunner{}},
-			},
-			{
-				Type:     logqueue.FlushLogQueue,
-				Schedule: "55 * * * * *",
-			},
-		},
-		Log: struct {
-			Format      string
-			Destination string
-		}{
-			Format:      "LogTypeFile",
-			Destination: "/var/log/speed-snitch",
-		},
+	for _, nextEntry := range entries {
+		thisCron.Remove(nextEntry.ID)
 	}
+
 }
