b72012ad96069e76e7a703ab890a5ab5f43dfec5
Steve Bagwell <baggerone@gmail.com>
2018-05-10 09:13:21 -0400

    If required, verify the signature of the upload file

--- lib/selfupdate/selfupdate.go
+++ lib/selfupdate/selfupdate.go
@@ -6,15 +6,17 @@ import (
 	"strings"
 	"os/exec"
 	"fmt"
-	"golang.org/x/crypto/openpgp"
 )
 
 const DefaultFileMode = 0755
-const GPGKeyFileName = "gpgPub.key"
+const SignedFileSuffix = ".sig"
 
 // UpdateIfNeeded checks current version and config version and if different downloads the version from config
 // If returns true, update occurred and process should be restarted, if false, check err, but if err is nil all is okay
-func UpdateIfNeeded(currentVersion, configVersion, downloadURL, signedDownloadURL string, verifySignature bool) (bool, error) {
+func UpdateIfNeeded(
+	currentVersion, configVersion, downloadURL string,
+	verifySignature bool,
+) (bool, error) {
 
 	if currentVersion != configVersion {
 		wd, _ := os.Getwd()
@@ -26,15 +28,16 @@ func UpdateIfNeeded(currentVersion, configVersion, downloadURL, signedDownloadUR
 		}
 
 		if verifySignature {
+			signedDownloadURL := downloadURL + SignedFileSuffix
 			signedFile := downloadFileBase + `-new.sig`
 			err := agent.DownloadFile(wd+"/"+signedFile, signedDownloadURL, DefaultFileMode)
 			if err != nil {
 				return false, fmt.Errorf("Error downloading signed file: %s", err.Error())
 			}
 
-			err = VerifyUpdateSignature(wd, downloadFile, signedFile)
+			err = agent.VerifyFileSignature(wd, downloadFile, signedFile)
 			if err != nil {
-				return false, err
+				return false, fmt.Errorf("Error verifying the binary's signature: %s", err.Error())
 			}
 		}
 
@@ -58,39 +61,4 @@ func getFilenameFromURL(URL string) string {
 	return parts[len(parts)-1]
 }
 
-func VerifyUpdateSignature(directory, targetFile, signedFile string) error {
-	keyFilePath := directory + "/" + GPGKeyFileName
-
-	_, err := os.Stat(keyFilePath)
-	if os.IsNotExist(err) {
-		return nil
-	}
-
-	keyRingReader, err := os.Open(keyFilePath)
-	if err != nil {
-		return err
-	}
-
-	signature, err := os.Open(signedFile)
-	if err != nil {
-		return err
-	}
-
-	verificationTarget, err := os.Open(targetFile)
-	if err != nil {
-		return err
-	}
-
-	keyring, err := openpgp.ReadArmoredKeyRing(keyRingReader)
-	if err != nil {
-		return fmt.Errorf("Error Reading Armored Key Ring: %s", err.Error())
-	}
-
-	_, err = openpgp.CheckArmoredDetachedSignature(keyring, verificationTarget, signature)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
 
