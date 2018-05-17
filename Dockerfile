4c0540553f7839713eea3e271889820d45c29f0c
Phillip Shipley <phillip.shipley@gmail.com>
2018-05-17 15:12:06 -0400

    refactored build process to build, sign, and deploy to s3, also adding codeship for automation

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM golang:latest
 
-RUN apt-get update -y && apt-get install -y git
+RUN apt-get update -y && apt-get install -y awscli git
 RUN go get -u github.com/golang/dep/cmd/dep
 RUN go get github.com/mitchellh/gox
+RUN wget -P /usr/local/bin/ https://raw.githubusercontent.com/silinternational/runny/0.2/runny && chmod a+x /usr/local/bin/runny
---
+++ build-sign-deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -x
+
+# array of target os/arch
+targets=( "darwin/amd64" "linux/amd64" "linux/arm" "windows/amd64" )
+distPath="../../dist"
+
+# download gpg keys to use for signing
+runny aws s3 cp s3://$KEY_BUCKET/secret.key ./
+runny gpg --import secret.key
+
+cd cmd/speedsnitch/
+for target in "${targets[@]}"
+do
+    # Build binary using gox
+    gox -osarch="${target}" -output="${distPath}/${target}/speedsnitch"
+
+    # If OS is windows, append .exe to filename before signing
+    if [ "${target}" == "windows/amd64" ]
+    then
+        fileToSign="${distPath}/${target}/speedsnitch.exe"
+    else
+        fileToSign="${distPath}/${target}/speedsnitch"
+    fi
+
+    # Sign file with GPG
+    runny gpg --yes -a -o "${fileToSign}.sig" --detach-sig $fileToSign
+
+done
+
+# Push dist/ to S3 under folder for CI_BRANCH (ex: develop or 1.2.3)
+cd ../..
+CI_BRANCH=${CI_BRANCH:="unknown"}
+aws s3 sync dist/ s3://$DOWNLOAD_BUCKET${DOWNLOAD_BUCKET_PATH}/$CI_BRANCH/
